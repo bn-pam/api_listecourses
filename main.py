b02115e3cb0a15b87dbc408ae9e5dd6fb4d0c3a0
bn-pam <bonneau.pam@gmail.com>
2024-06-12 10:11:31 +0200

    fix:

--- main.py
+++ main.py
@@ -4,14 +4,35 @@ ma_liste_courses = {}
 
 app = FastAPI()
 
-@app.get("/liste")
+@app.get("/liste") #un dictionnaire de dictionnaires
 def get_list():
     return {"content": ma_liste_courses}
 
 @app.post("/liste")
 def add_to_list(produit: str, quantite: float, unite: str):
-    ma_liste_courses[produit] = {"quantite": quantite, "unite": unite}
-    return {"content": ma_liste_courses}
+    # vérifier si l'élément est déjà dans le dictionnaire
+    if produit in ma_liste_courses : #vérifier que la clé qui est présentée, correspond à une clé du dico, pour parcourir un dictionnaire 
+        # s'il est dans le dico, vérifier que l'unité est la même 
+            if unite:
+                if unite == ma_liste_courses[produit][1]: #on fait matcher unité cherchée et unité du produit donné plus haut
+                #pour obtenir le dico : on donne la clé et on obtient la valeur
+                # si l'unité est la même on ajoute les unités
+                    ma_liste_courses[produit][0]+=quantite
+                    return {produit:ma_liste_courses[produit]} #renvoyer le produit et sa quantité cad la valeur correspondant à la clé produit
+                else :  # si l'unité est pas la même, on renvoie un message d'erreur
+                    raise HTTPException(
+                        status_code=400,
+                        detail=f"Not the good unit for the product, {produit} is in {ma_liste_courses[produit]}"
+                    ) 
+                # pas d'unité fournie, j'ajoute par défault
+            else:
+                ma_liste_courses[produit][0]+=quantite
+                return {produit:ma_liste_courses[produit]}
+    # si non, on ajoute l'élément au dictionnaire
+    else : 
+        ma_liste_courses[produit] = [quantite, unite]
+        return {produit:ma_liste_courses[produit]}
+
 
 @app.delete("/liste")
 def remove_from_list(produit: str, quantite: float, unite: str):
